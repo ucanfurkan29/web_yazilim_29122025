2a42b979ffcd2eb68ec5c3a44cbd4b8b5df9a429
ucanfurkan29 <ucanfurkan1828@gmail.com>
2026-01-19 16:08:34 +0300

    Ders Sonu

--- 07_custom_helpers/Helpers/StringHelper.cs
+++ 07_custom_helpers/Helpers/StringHelper.cs
@@ -50,6 +50,36 @@
             Array.Reverse(chars);
             return new string(chars);
         }
+        public static int ParametreUzunlugu(object input)
+        {
+            if (input == null)
+                return 0;
+
+            // String öncelikli (string ayrıca IEnumerable<char> olduğu için önce kontrol ediyoruz)
+            if (input is string s)
+                return s.Length;
+
+            // Diziler için hızlı yol
+            if (input is Array arr)
+                return arr.Length;
+
+            // Koleksiyonlar için Count kullan
+            if (input is System.Collections.ICollection coll)
+                return coll.Count;
+
+            // Diğer IEnumerable türleri için say
+            if (input is System.Collections.IEnumerable enumerable)
+            {
+                int count = 0;
+                var enumerator = enumerable.GetEnumerator();
+                while (enumerator.MoveNext())
+                    count++;
+                return count;
+            }
+
+            // Diğer nesneler için ToString() uzunluğunu döndür
+            return input.ToString()?.Length ?? 0;
+        }
 
     }
 }
