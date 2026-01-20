78d55711d03a2933b22a5f8adc9936dbca1a8100
ucanfurkan29 <ucanfurkan1828@gmail.com>
2026-01-20 15:12:34 +0300

    08 - 09 konuları

---
+++ 09_render_section_nested/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _09_render_section_nested.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ 09_render_section_nested/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _09_render_section_nested.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
